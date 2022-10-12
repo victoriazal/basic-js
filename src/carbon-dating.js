ebbf4728688bc42b192a2cf9e915e3f25ad73851
victoriazal <victoriazalomnova19@gmail.com>
2022-10-12 23:55:18 +0300

    feat: solve carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if( typeof(sampleActivity) !='number' && !isNaN(sampleActivity) && Number(sampleActivity) > 0 && Number(sampleActivity)!= 0 && sampleActivity != '' && typeof(sampleActivity) != 'object'){
+    let numAnswer = Math.ceil((Math.log(15/Number(sampleActivity)))/ (0.693/5730))
+    answer = numAnswer
+    if(answer <0 || typeof(answer) != "number" || !answer){
+      answer = false
+    }
+    }
+  else {
+    answer = false
+  }
+  return answer
 }
 
 module.exports = {
