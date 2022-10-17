3e4d57dd5db0944a3e5e04be47a2628603d62beb
victoriazal <victoriazalomnova19@gmail.com>
2022-10-17 17:39:34 +0300

    feat:start transform arr

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  console.log(typeof(arr[0]))
+  let index 
+  arr.map(item => {
+    if(typeof(item) == 'string'){
+      console.log(item)
+      index = arr.indexOf(item)
+    }
+  })
+  
+  let newArr = arr.sort().splice(1)
+  let rule = arr.sort().slice(0,1).toString()
+  console.log(rule)
+  if(rule == '--double-next'){
+    newArr.splice(index,0,newArr[index])
+  }
+  else if(rule == '--double-prev'){
+    newArr.splice(index-1,0,newArr[index-1])
+  }
+  else if(rule == '--discard-prev'){
+    newArr.splice(index-1,1)
+  }
+  else if(rule == '--discard-next'){
+    newArr.splice(index,1)
+  }
+  return newArr
+  }
 
 module.exports = {
   transform
